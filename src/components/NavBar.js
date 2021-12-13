dff3413f8c702ae845d3c79a814288ba128321c6
MisterZcookie <gilmoreira14@gmail.com>
2021-12-13 20:11:50 +0000

    a

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,23 +9,17 @@ import {makeStyles} from "@material-ui/core/styles";
 const styles = makeStyles({
     bar:{
         paddingTop: "1.15rem",
-        backgroundColor: "#fff",
-        ['@media (max-width:780px)']: { 
-           flexDirection: "column"
-          }
+        backgroundColor: "#fff"
+        
     },
     logo: {
         width: "15%", 
-        ['@media (max-width:780px)']: { 
-           display: "none"
-           }
+        
     },
     logoMobile:{
         width: "100%", 
         display: "none", 
-        ['@media (max-width:780px)']: { 
-            display: "inline-block"
-            }
+        
     },
     menuItem: {
         cursor: "pointer", 
@@ -33,8 +27,7 @@ const styles = makeStyles({
         "&:hover": {
             color:  "#4f25c8"
         },
-        ['@media (max-width:780px)']: { 
-            paddingBottom: "1rem"    }
+        
     }
 })
 
@@ -42,8 +35,8 @@ function NavBar() {
     const classes = styles()
     return (
             <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>   
-                <img src={logo} className={classes.logo}/> 
-                <img src={logoMobile} className={classes.logoMobile}/> 
+                <img src={logo} className={classes.logo} alt=""/> 
+                <img src={logoMobile} className={classes.logoMobile} alt=""/> 
                 <Typography variant="h6" className={classes.menuItem}>
                    About
                 </Typography>
